e3148c77b4119ff749fa0786296df612374a814f
sarveshwar22 <56260368+sarveshwar22@users.noreply.github.com>
2022-03-25 16:08:38 +0530

    Time: 3 ms (88.32%), Space: 7.3 MB (40.63%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
     int longestValidParentheses(string s) {
-        int n=s.size();
-        if(n<=1)
-            return 0;
-        vector<int>dp(n,0);
-        int maxAns=0;
-        for(int i=1;i<n;i++)
+        stack<int>st;
+        st.push(-1);
+        int ans=0;
+        for(int i=0;i<s.size();i++)
         {
-            if(s[i]==')')
+            if(s[i]=='(')
             {
-                if(s[i-1]=='(')
+                st.push(i);
+            }
+            else
+            {
+                st.pop();
+                if(!st.empty())
                 {
-                    dp[i]=((i-2)>=0?dp[i-2]:0)+2;
-                    maxAns=max(maxAns,dp[i]);
+                    ans=max(ans,i-st.top());
                 }
                 else
                 {
-                    if((i-1)-dp[i-1]>=0 && s[(i-1)-dp[i-1]]=='(' ){
-                        dp[i]=dp[i-1]+2+((i-1-dp[i-1]-1)>=0 ? dp[i-1-dp[i-1]-1]:0);
-                        maxAns=max(maxAns,dp[i]);
-                    }
+                    st.push(i);
                 }
-
             }
         }
-        return maxAns;
+        return ans; 
     }
 };
