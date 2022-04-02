c81e4d9bcc2e35129b13e3c16c48a64256a0515c
sarveshwar22 <56260368+sarveshwar22@users.noreply.github.com>
2022-04-02 21:43:07 +0530

    Time: 74 ms (40.41%), Space: 27.2 MB (12.21%) - LeetHub

---
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+    bool validPalindrome(string s,int low,int high,int &k)
+    {
+        while(low<high)
+        {
+            if(s[low]==s[high])
+            {
+                low++;
+                high--;
+            }
+            else
+            {
+                k--;
+                if(k<0)
+                    return false;
+                return validPalindrome(s,low+1,high,k)||validPalindrome(s,low,high-1,k);
+            }
+        }
+        return true;
+    }
+public:
+    bool validPalindrome(string s) {
+        int n=s.size();
+        int low=0;
+        int high=n-1;
+        int k=1;
+        return validPalindrome(s,low,high,k);
+    }
+};
