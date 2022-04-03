babf781ac1caf264a14b8dac561614b3527efaa4
sarveshwar22 <56260368+sarveshwar22@users.noreply.github.com>
2022-04-03 14:53:54 +0530

    Time: 28 ms (59.71%), Space: 14 MB (55.30%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.cpp
+++ 210-course-schedule-ii/210-course-schedule-ii.cpp
@@ -1,3 +1,58 @@
+// BFS solution-
+
+// class Solution {
+//     vector<vector<int>> buildGraph(int numCourses, vector<vector<int>>& prerequisites)
+//     {
+//         vector<vector<int>>graph(numCourses);
+//         for(auto& prereq:prerequisites)
+//         {
+//             graph[prereq[1]].push_back(prereq[0]);
+//         }
+//         return graph;
+//     }
+//     vector<int> computeIndegrees(vector<vector<int>>& graph)
+//     {
+//         vector<int> indegrees(graph.size(),0);
+//         for(auto& adj:graph)
+//         {
+//             for(int neigh:adj)
+//             {
+//                 indegrees[neigh]++;
+//             }
+//         }
+//         return indegrees;
+//     }
+    
+    
+// public:
+//     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+//         vector<int> res;
+//         vector<vector<int>> graph=buildGraph(numCourses,prerequisites);
+//         vector<int> inDegree=computeIndegrees(graph);
+//         vector<int> order;
+//         for(int i=0;i<numCourses;i++)
+//         {
+//             int j=0;
+//             for(;j<numCourses;j++)
+//             {
+//                 if(!inDegree[j])
+//                 {
+//                     order.push_back(j);
+//                     break;
+//                 }
+//             }
+//             if(j==numCourses)return {};
+//             inDegree[j]--;
+//             for(int neigh:graph[j])
+//             {
+//                 inDegree[neigh]--;
+//             }
+//         }
+//         return order;
+//     }
+// };
+
+// DFS solution-
 class Solution {
     bool isCyclic(int node,vector<vector<int>>&adj,vector<bool>&visited,vector<bool>&vis2)
     {
