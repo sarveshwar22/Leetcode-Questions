3656ae7c3421281f154ea6e61a121e3ff8a7b935
sarveshwar22 <56260368+sarveshwar22@users.noreply.github.com>
2022-04-03 07:33:20 +0530

    Time: 15 ms (86.16%), Space: 15.6 MB (5.38%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -7,18 +7,37 @@ public:
         unordered_map<int,int>freq;
         for(auto& num:nums)
             freq[num]++;
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        // priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        // for(auto& ele:freq)
+        // {
+        //     pq.push({ele.second,ele.first});
+        //     if(pq.size()>k)
+        //         pq.pop();
+        // }
+        // vector<int>res(k);
+        // for(int i=0;i<k;i++)
+        // {
+        //     res[i]=pq.top().second;
+        //     pq.pop();
+        // }
+        
+        vector<int>res;
+        vector<vector<int>> bucket(n+1);
+        int maxi=INT_MIN;
         for(auto& ele:freq)
         {
-            pq.push({ele.second,ele.first});
-            if(pq.size()>k)
-                pq.pop();
+            bucket[ele.second].push_back(ele.first);
+            maxi=max(maxi,ele.second);
         }
-        vector<int>res(k);
-        for(int i=0;i<k;i++)
+        
+        int curr=maxi;
+        while(res.size()<k)
         {
-            res[i]=pq.top().second;
-            pq.pop();
+            if(!bucket.empty())
+            {
+                res.insert(res.end(),bucket[curr].begin(),bucket[curr].end());
+                curr--;
+            }
         }
         return res;
     }
