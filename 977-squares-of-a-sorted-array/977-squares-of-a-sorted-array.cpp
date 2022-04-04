0c6a43a866d533a73a9a371a79e2862e5d229ace
sarveshwar22 <56260368+sarveshwar22@users.noreply.github.com>
2022-04-05 03:44:59 +0530

    Time: 48 ms (47.12%), Space: 25.8 MB (82.06%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
@@ -1,14 +1,24 @@
 class Solution {
-    static bool cmp(int a,int b)
-    {
-        return a*a<b*b;
-    }
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        vector<int> res;
-        sort(nums.begin(),nums.end(),cmp);
-        for(int i=0;i<nums.size();i++)
-            res.push_back(nums[i]*nums[i]);
+        int n=nums.size();
+        int l=0,r=n-1;
+        vector<int>res(n);
+        int i=0;
+        while(l<=r)
+        {
+            if(abs(nums[l])>abs(nums[r]))
+            {
+                res[n-i-1]=nums[l]*nums[l];
+                l++;
+            }
+            else
+            {
+                res[n-i-1]=nums[r]*nums[r];
+                r--;
+            }
+            i++;
+        }
         return res;
     }
 };
