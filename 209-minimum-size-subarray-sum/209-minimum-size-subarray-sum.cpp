5c45fb13323da2119f06335495ecfbdb51c7bbbd
sarveshwar22 <56260368+sarveshwar22@users.noreply.github.com>
2022-04-06 22:33:00 +0530

    Time: 19 ms (14.81%), Space: 10.8 MB (6.41%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
@@ -1,20 +1,34 @@
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
-        int l=0;
-        int r=0;
-        int sum=0;
+        // int l=0;
+        // int r=0;
+        // int sum=0;
+        // int n=nums.size();
+        // int length=INT_MAX;
+        // while(r<n)
+        // {
+        //     sum+=nums[r++];
+        //     while(sum>=target)
+        //     {
+        //         length=min(r-l,length);
+        //         sum-=nums[l++];
+        //     }
+        // }
+        // return length==INT_MAX?0:length;
+        
         int n=nums.size();
-        int length=INT_MAX;
-        while(r<n)
+        vector<int> csum(n+1,0);
+        int res=INT_MAX;
+        for(int i=1;i<=n;i++)
         {
-            sum+=nums[r++];
-            while(sum>=target)
-            {
-                length=min(r-l,length);
-                sum-=nums[l++];
-            }
+            csum[i]=csum[i-1]+nums[i-1];
         }
-        return length==INT_MAX?0:length;
+        for(int i=n;i>=0 && csum[i]>=target;i--)
+        {
+            int j=upper_bound(csum.begin(),csum.end(),csum[i]-target)-csum.begin();
+            res=min(res,i-j+1);
+        }
+        return res==INT_MAX?0:res;
     }
 };
