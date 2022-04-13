468ea7803cbc24fc197e914dd7f5dab7208cf00d
sarveshwar22 <56260368+sarveshwar22@users.noreply.github.com>
2022-04-13 17:04:42 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (63.66%) - LeetHub

---
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+    public:
+        vector<vector<int> > generateMatrix(int n) {
+            vector<vector<int>>res(n,vector<int>(n));
+        	int k = 1, i = 0;
+        	while(k<=n*n)
+        	{
+        		int j = i;
+        		while(j<n-i)     
+        			res[i][j++]=k++;
+        		j=i+1;
+        		while(j<n-i)
+        			res[j++][n-i-1]=k++;
+        		j=n-i-2;
+        		while(j>i)
+        			res[n-i-1][j--]=k++;
+        		j = n - i - 1;
+        		while(j>i)               
+        			res[j--][i]=k++;
+        		i++;   
+        	}
+        	return res;
+        }
+    };
