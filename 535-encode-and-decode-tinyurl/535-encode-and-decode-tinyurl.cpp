f378ec5eedd7bd5ef30ef2138a4715cda7779c76
sarveshwar22 <56260368+sarveshwar22@users.noreply.github.com>
2022-04-23 07:55:25 +0530

    Time: 3 ms (83.88%), Space: 7.1 MB (45.21%) - LeetHub

--- 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
@@ -1,14 +1,20 @@
 class Solution {
+    unordered_map<string,string> m;
+    
 public:
 
     // Encodes a URL to a shortened URL.
     string encode(string longUrl) {
-        return longUrl;
+        int hashSize= m.size();
+        string tt= to_string(hashSize+1);
+        m[tt]=longUrl;
+        return "http://tinyurl.com/"+tt;
     }
 
     // Decodes a shortened URL to its original URL.
     string decode(string shortUrl) {
-        return shortUrl;
+        cout<<shortUrl.substr(19);
+        return m[shortUrl.substr(19)];
     }
 };
 
