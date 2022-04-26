02cf19a9c77f580a8a38aa6c1d78ea30305310b6
sarveshwar22 <56260368+sarveshwar22@users.noreply.github.com>
2022-04-27 04:45:45 +0530

    Time: 61 ms (99.61%), Space: 10.3 MB (93.38%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,43 +1,178 @@
-class Solution {
-    int getParent(vector<int>& parents,int i)
-    {
-        if(parents[i]==i)
-            return i;
-        return parents[i]=getParent(parents,parents[parents[i]]);
-    }
+class UnionFind {
 public:
-    int minCostConnectPoints(vector<vector<int>>& points) {
-        int n=points.size();
-        vector<int> parent(n);
-        iota(parent.begin(),parent.end(),0);
-        int ans=0;
-        priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>>pq;
+    vector<int> root, rank;
+    
+    UnionFind(int sz) : root(sz), rank(sz, 1) {
+        iota(root.begin(), root.end(), 0);
+    }
+    
+    int find(int x) {
+        if (x == root[x]) {
+            return x;
+        }
+        return root[x] = find(root[x]);
+    }
+    
+    void unite(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX != rootY) {
+            if (rank[rootX] < rank[rootY]) {
+                root[rootX] = rootY;
+            } else if (rank[rootX] > rank[rootY]) {
+                root[rootY] = rootX;
+            } else {
+                root[rootY] = rootX;
+                rank[rootX]++;
+            }
+        }
+    }
+    
+    bool isConnected(int x, int y) {
+        return find(x) == find(y);
+    }
+};
+
+class Solution {
+public:
+    int manhattan(vector<int>& a, vector<int>& b) {
+        return abs(a[0] - b[0]) + abs(a[1] - b[1]);
+    }
+    
+    int kruskal(vector<vector<int>>& points) {
+        int V = points.size();
+        vector<tuple<int, int, int>> edges;
         
-        for(int i=0;i<n;i++)
-        {
-            for(int j=i+1;j<n;j++)
-            {
-                if(i!=j)
-                {
-                    pq.push({abs(points[i][0]-points[j][0])+abs(points[i][1]-points[j][1]),i,j});
+        for (int i = 0; i < V - 1; i++) {
+            for (int j = i + 1; j < V; j++) {
+                edges.push_back({manhattan(points[i], points[j]), i, j});
+            }
+        }
+        
+        sort(edges.begin(), edges.end());
+        
+        UnionFind uf(V);
+        
+        int cost = 0;
+        int E = 0;
+        for (auto& edge : edges) {
+            if (not uf.isConnected(get<1>(edge), get<2>(edge))) {
+                uf.unite(get<1>(edge), get<2>(edge));
+                cost += get<0>(edge);
+                E++;
+                if (E == V - 1) {
+                    break;
                 }
             }
         }
         
-        for(int i=0;i<n-1;)
-        {
-            vector<int> edge = pq.top();
+        return cost;
+    }
+    
+    int prim(vector<vector<int>>& points) {
+        int V = points.size();
+        vector<bool> used(V, false);
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        pq.push({0, 0});
+        
+        int cost = 0;
+        int E = 0;
+        while (E < V) {
+            auto [weight, node] = pq.top();
             pq.pop();
-            int p1=getParent(parent,edge[1]);
-            int p2=getParent(parent,edge[2]);
-            if(p1!=p2)
-            {
-                ans+=edge[0];
-                parent[p1]=p2;
-                i++;
+            
+            if (used[node]) {
+                continue;
+            }
+            
+            used[node] = true;
+            cost += weight;
+            E++;
+            
+            for (int i = 0; i < V; i++) {
+                if (used[i]) {
+                    continue;
+                }
+                pq.push({manhattan(points[i], points[node]), i});
             }
         }
-        return ans;
         
+        return cost;
+    }
+    
+    int primOpt(vector<vector<int>>& points) {
+        int V = points.size();
+        vector<bool> used(V, false);
+        vector<int> minDist(V, INT_MAX);
+        minDist[0] = 0;
+        
+        int cost = 0;
+        int E = 0;
+        
+        while (E < V) {
+            int minWeight = INT_MAX;
+            int minNode = -1;
+            
+            for (int i = 0; i < V; i++) {
+                if (not used[i] and minDist[i] < minWeight) {
+                    minWeight = minDist[i];
+                    minNode = i;
+                }
+            }
+            
+            used[minNode] = true;
+            cost += minWeight;
+            E++;
+            
+            for (int i = 0; i < V; i++) {
+                if (not used[i]) {
+                    minDist[i] = min(minDist[i], manhattan(points[i], points[minNode]));
+                }
+            }
+        }
+        
+        return cost;
+    }
+    
+    int primOptList(vector<vector<int>>& points) {
+        int V = points.size();
+        list<pair<int, int>> minDist;
+        for (int i = 1; i < V; i++) {
+            minDist.push_back({i, INT_MAX});
+        }
+        minDist.push_front({0, 0});
+        
+        int cost = 0;
+        int E = 0;
+        
+        while (E < V) {
+            auto minIt = minDist.end();
+            int minWeight = INT_MAX;
+            
+            for (auto it = minDist.begin(); it != minDist.end(); it++) {
+                if (it->second < minWeight) {
+                    minWeight = it->second;
+                    minIt = it;
+                }
+            }
+            
+            int minNode = minIt->first;
+            cost += minIt->second;
+            E++;
+            minDist.erase(minIt);
+            
+            for (auto it = minDist.begin(); it != minDist.end(); it++) {
+                it->second = min(it->second, manhattan(points[it->first], points[minNode]));
+            }
+        }
+        
+        return cost;
+    }
+    
+    int minCostConnectPoints(vector<vector<int>>& points) {
+        // return kruskal(points);
+        // return prim(points);
+        // return primOpt(points);
+        return primOptList(points);
     }
 };
