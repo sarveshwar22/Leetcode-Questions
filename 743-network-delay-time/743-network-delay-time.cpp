606903cbaac804c6c161c29f779d17ec79815f35
sarveshwar22 <56260368+sarveshwar22@users.noreply.github.com>
2022-05-15 04:18:57 +0530

    Time: 123 ms (87.55%), Space: 39.8 MB (90.00%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -1,20 +1,39 @@
 class Solution {
+    
 public:
-    int networkDelayTime(vector<vector<int>>& times, int N, int K) {
-        vector<int> dist(N + 1, INT_MAX);
-        dist[K] = 0;
-        for (int i = 0; i < N; i++) {
-            for (vector<int> e : times) {
-                int u = e[0], v = e[1], w = e[2];
-                if (dist[u] != INT_MAX && dist[v] > dist[u] + w) {
-                    dist[v] = dist[u] + w;
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        
+        vector<vector<pair<int,int>>>adj(n+1);
+        int m=times.size();
+        for(int i=0;i<m;i++)
+        {
+            adj[times[i][0]].push_back({times[i][1],times[i][2]});
+        }
+        vector<int>dist(n+1,INT_MAX);
+        dist[k]=0;
+        
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        pq.push({0,k});
+        
+        while(!pq.empty())
+        {
+            auto curr=pq.top();
+            pq.pop();
+            for(auto it:adj[curr.second])
+            {
+                if(dist[it.first]>curr.first+it.second)
+                    {dist[it.first]=curr.first+it.second;
+                    pq.push({dist[it.first],it.first});
                 }
             }
         }
-
-        int maxwait = 0;
-        for (int i = 1; i <= N; i++)
-            maxwait = max(maxwait, dist[i]);
-        return maxwait == INT_MAX ? -1 : maxwait;
+        int ans=0;
+        for(int i=1;i<n+1;i++)
+        {
+            if(dist[i]==INT_MAX)
+                return -1;
+            ans=max(ans,dist[i]);
+        }
+        return ans;
     }
 };
