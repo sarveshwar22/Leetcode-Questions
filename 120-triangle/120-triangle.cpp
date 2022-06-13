03b7e6a980482ff8712be7e2e179d84994655992
sarveshwar22 <56260368+sarveshwar22@users.noreply.github.com>
2022-06-13 13:23:46 +0530

    Time: 7 ms (75.60%), Space: 9.2 MB (9.59%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,21 +1,35 @@
 class Solution {
+    int Total(vector<vector<int>>& triangle,int i,int r,vector<vector<int>>& memo)
+    {
+        if(r==triangle.size())
+            return 0; 
+        if(memo[r][i]!=-1)
+            return memo[r][i];
+        int res=min(Total(triangle,i,r+1,memo),Total(triangle,i+1,r+1,memo))+triangle[r][i];
+        return memo[r][i]=res;
+    }
+    
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
-		int n = triangle.size();
-		vector<int>next(n);
-		for(int i = n-1; i >= 0; i--) {
-			vector<int>curr(n);
-			for(int j = i; j >= 0; j--) {
-				if(i == n-1)
-					curr[j] = triangle[i][j];
-				else {
-					int up = triangle[i][j] + next[j];
-					int up_left = triangle[i][j] + next[j+1];
-					curr[j] = min(up, up_left);
-				}
-			}
-			next = curr;
-		}
-		return next[0];
-	}
+        int n=triangle.size();
+        // vector<vector<int>> memo(n,vector<int>(n,-1));
+        // return Total(triangle,0,0,memo);
+        vector<vector<int>> memo(n,vector<int>(n,0));
+        memo[0][0]=triangle[0][0];
+        for(int i=1;i<n;i++)
+        {
+            for(int j=0;j<triangle[i].size();j++)
+            {
+                if(j==0)
+                    memo[i][j]=memo[i-1][j]+triangle[i][j];
+                else if(j==triangle[i].size()-1)
+                    memo[i][j]=memo[i-1][j-1]+triangle[i][j];
+                else 
+                    memo[i][j]=min(memo[i-1][j-1],memo[i-1][j])+triangle[i][j];
+            }
+        }
+        
+        return *min_element(memo[n-1].begin(),memo[n-1].end());
+        
+    }
 };
