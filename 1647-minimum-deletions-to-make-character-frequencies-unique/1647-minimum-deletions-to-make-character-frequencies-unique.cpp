53ae32a4735e23cd068db4cf75c9980ff93d51d6
sarveshwar22 <56260368+sarveshwar22@users.noreply.github.com>
2022-06-28 19:58:20 +0530

    Time: 69 ms (72.02%), Space: 17.3 MB (81.59%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -1,38 +1,27 @@
 class Solution {
 public:
     int minDeletions(string s) {
-        map <char,int> count;
-        for (auto &c: s) count[c]++;
-        map <int,int> mp;
-        int mmax=0;
-        for (auto p: count){ 
-            mp[p.second]++;
-            mmax=max(mmax,p.second);
+        vector<int> frequency(26, 0);
+        for (char c : s) {
+            frequency[c - 'a']++;
         }
-        deque <int> dq;
-        int ans=0;
-        int i=1;
-        while (i<=mmax){
-            
-            if (mp[i]==0) {
-                dq.push_back(i);
-                i++;
-                continue;
+        
+        sort(frequency.begin(), frequency.end(), greater<int>());
+        
+        int deleteCount = 0;
+        int maxFreqAllowed = s.size();
+    
+        for (int i = 0; i < 26 && frequency[i] > 0; i++) {
+            if (frequency[i] > maxFreqAllowed) {
+                deleteCount += frequency[i] - maxFreqAllowed;
+                frequency[i] = maxFreqAllowed;
             }
-            if (mp[i]==1) {
-                i++;
-                continue;
-            }
-            
-            while (!dq.empty() && mp[i]>1){
-                int val=dq.back();
-                dq.pop_back();
-                ans+=i-val;
-                mp[i]--;
-            }
-            if (mp[i]>1) ans+=(mp[i]-1)*i;
-            i++;
+            maxFreqAllowed = max(0, frequency[i] - 1);
         }
-        return ans;
+        
+        return deleteCount;
     }
 };
+
+static bool     _foo = ios::sync_with_stdio(false);
+static ostream* _bar = cin.tie(NULL);
