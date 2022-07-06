6edc14a504af6a48161a2048f048003f6c8ab54b
sarveshwar22 <56260368+sarveshwar22@users.noreply.github.com>
2022-07-06 16:05:23 +0530

    Time: 3 ms (50.16%), Space: 7.2 MB (82.74%) - LeetHub

--- 140-word-break-ii/140-word-break-ii.cpp
+++ 140-word-break-ii/140-word-break-ii.cpp
@@ -1,9 +1,6 @@
 class Solution {
-    // unordered_map<string, vector<string>> m;
 public:
     vector<string> wordBreak(string s, vector<string>& wordDict) {
-	    // if(m.find(s) != m.end())
-	    // return m[s];
 	    vector<string> res;
 	    for(auto word : wordDict){
 		    if(s.substr(0, word.size()) == word){          
@@ -17,7 +14,6 @@ public:
 			    }
 		    }
 	    }
-	// m[s] = res;
 	return res;
 }
 };
