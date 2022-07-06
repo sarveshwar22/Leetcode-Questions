617cc5b3922727bc1eaf57a994822b2481256761
sarveshwar22 <56260368+sarveshwar22@users.noreply.github.com>
2022-07-07 01:27:17 +0530

    Time: 745 ms (15.97%), Space: 63.5 MB (41.68%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.cpp
+++ 630-course-schedule-iii/630-course-schedule-iii.cpp
@@ -5,16 +5,16 @@ public:
         sort(courses.begin(), courses.end(), [](const vector<int>& a, vector<int>& b) {
             return a[1] < b[1];
         });
-        priority_queue<int> q;
+        priority_queue<int> pq;
         int sum = 0;
-        for(auto i : courses) {
-            sum += i[0];
-            q.push(i[0]);
-            if(sum > i[1]) {
-                sum -= q.top();
-                q.pop();
+        for(auto course : courses) {
+            sum += course[0];
+            pq.push(course[0]);
+            if(sum > course[1]) {
+                sum -= pq.top();
+                pq.pop();
             }
         }
-        return q.size();
+        return pq.size();
     }
 };
