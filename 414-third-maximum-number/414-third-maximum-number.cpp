e9afcd48e97b3eceffe50015f74a393f11364fb2
sarveshwar22 <56260368+sarveshwar22@users.noreply.github.com>
2022-07-07 17:15:21 +0530

    Time: 9 ms (64.97%), Space: 9.1 MB (53.36%) - LeetHub

--- 414-third-maximum-number/414-third-maximum-number.cpp
+++ 414-third-maximum-number/414-third-maximum-number.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
     int thirdMax(vector<int>& nums) {
-        long long int highest=(long long int)INT_MIN-1;
-        long long int secondHighest=(long long int)INT_MIN-1;
-        long long int thirdHighest=(long long int)INT_MIN-1;
-        
-        for(int i=0;i<nums.size();i++)
+        long long one = (long long)INT_MIN-1;
+        long long two = (long long)INT_MIN-1;
+        long long three = (long long)INT_MIN-1;
+        bool flag = false;
+        for(int i = 0; i < nums.size();i++)
         {
-            if(nums[i]>highest)
-                highest = nums[i];
-        }
-        for(int i=0;i<nums.size();i++)
-        {
-            if(nums[i]>secondHighest && nums[i]!=highest)
-                secondHighest = nums[i];
-        }
-        for(int i=0;i<nums.size();i++)
-        {
-            if(nums[i]>thirdHighest && nums[i]!=highest && nums[i]!=secondHighest)
-                thirdHighest = nums[i];
-        }
-        
-        if(thirdHighest == (long long int)INT_MIN-1)
-            return highest;
-        return thirdHighest;
+            if(nums[i] > one)
+            {
+                three = two;
+                two = one;
+                one = nums[i];
+            }
+            else if(nums[i] > two and nums[i]!= one)
+            {
+                three = two;
+                two = nums[i];
+            }
+            else if(nums[i] > three and nums[i]!=one and nums[i]!=two)
+            {
+                three = nums[i];
+            }
+         }
+        if(three == (long long)INT_MIN -1) return one;
+        return three;
     }
 };
