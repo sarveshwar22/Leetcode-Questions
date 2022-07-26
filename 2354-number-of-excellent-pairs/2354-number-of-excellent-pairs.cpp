df9ebc207b01cde36f1c138bb8d66bdc5eaccf22
sarveshwar22 <56260368+sarveshwar22@users.noreply.github.com>
2022-07-27 00:35:57 +0530

    Time: 277 ms (100.00%), Space: 86.7 MB (50.00%) - LeetHub

--- 2354-number-of-excellent-pairs/2354-number-of-excellent-pairs.cpp
+++ 2354-number-of-excellent-pairs/2354-number-of-excellent-pairs.cpp
@@ -6,6 +6,7 @@ public:
         long long res=0;
         for(int num:unordered_set(nums.begin(),nums.end()))
             cnt[__builtin_popcount(num)]++;
+        
         for(int i=1;i<30;i++)
         {
             for(int j=1;j<30;j++)
