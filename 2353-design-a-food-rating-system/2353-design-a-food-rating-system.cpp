962192e8c359cbee9a01a598a883e75d51eccee8
sarveshwar22 <56260368+sarveshwar22@users.noreply.github.com>
2022-07-27 00:29:24 +0530

    Time: 697 ms (66.67%), Space: 166 MB (33.33%) - LeetHub

--- 2353-design-a-food-rating-system/2353-design-a-food-rating-system.cpp
+++ 2353-design-a-food-rating-system/2353-design-a-food-rating-system.cpp
@@ -1,6 +1,14 @@
+class compare{
+    public:
+    bool operator()(const pair<int,string>&p1,const pair<int,string>&p2)const{
+        if(p1.first==p2.first)
+            return p1.second<p2.second;
+        return p1.first>p2.first;
+    }
+};
 
 class FoodRatings {
-    unordered_map<string,set<pair<int,string>>> cuisine_rating;
+    unordered_map<string,set<pair<int,string>,compare> > cuisine_rating;
     unordered_map<string,string> food_cuisine;
     unordered_map<string,int> food_rating;
 public:
@@ -8,7 +16,7 @@ public:
         int n=foods.size();
         for(int i=0;i<n;i++)
         {
-            cuisine_rating[cuisines[i]].insert({-ratings[i],foods[i]});
+            cuisine_rating[cuisines[i]].insert({ratings[i],foods[i]});
             food_cuisine[foods[i]] = cuisines[i];
             food_rating[foods[i]] = ratings[i];
         }
@@ -16,8 +24,8 @@ public:
     
     void changeRating(string food, int newRating) {
         auto& cuisine = food_cuisine.find(food)->second;
-        cuisine_rating[cuisine].erase({-food_rating[food],food});
-        cuisine_rating[cuisine].insert({-newRating,food});
+        cuisine_rating[cuisine].erase({food_rating[food],food});
+        cuisine_rating[cuisine].insert({newRating,food});
         food_rating[food] = newRating;
     }
     
