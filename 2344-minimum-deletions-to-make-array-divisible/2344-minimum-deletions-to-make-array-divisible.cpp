41800bedf2116f2cd2c2cee301eccda71f202b1b
sarveshwar22 <56260368+sarveshwar22@users.noreply.github.com>
2022-07-28 20:42:35 +0530

    Time: 294 ms (25.00%), Space: 88.2 MB (25.00%) - LeetHub

--- 2344-minimum-deletions-to-make-array-divisible/2344-minimum-deletions-to-make-array-divisible.cpp
+++ 2344-minimum-deletions-to-make-array-divisible/2344-minimum-deletions-to-make-array-divisible.cpp
@@ -1,26 +1,36 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, vector<int>& numsDivide) {
+        int gcd = numsDivide[0];
+        for(int num:numsDivide)
+            gcd = __gcd(gcd,num);
         sort(nums.begin(),nums.end());
-        int i=0;
-        for(;i<nums.size();i++)
+        for(int i=0;i<nums.size();i++)
         {
-            if(i!=0)
-            {
-                if(nums[i]==nums[i-1])
-                    continue;
-            }
-            for(int num:numsDivide)
-            {
-                if(num%nums[i])
-                    goto x;
-            }
-            break;
-            x:;
-            
+            if(!(gcd%nums[i]))
+                return i;
         }
-        if(i==nums.size())
-            return -1;
-        return i;
+        return -1;
+//         sort(nums.begin(),nums.end());
+//         int i=0;
+//         for(;i<nums.size();i++)
+//         {
+//             if(i!=0)
+//             {
+//                 if(nums[i]==nums[i-1])
+//                     continue;
+//             }
+//             for(int num:numsDivide)
+//             {
+//                 if(num%nums[i])
+//                     goto x;
+//             }
+//             break;
+//             x:;
+            
+//         }
+//         if(i==nums.size())
+//             return -1;
+//         return i;
     }
 };
