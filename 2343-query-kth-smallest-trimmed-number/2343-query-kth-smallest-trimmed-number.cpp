dedbe2d19de2db7157bde0885f02ee4653329cd4
sarveshwar22 <56260368+sarveshwar22@users.noreply.github.com>
2022-07-28 21:31:51 +0530

    Time: 2140 ms (11.11%), Space: 239.6 MB (11.11%) - LeetHub

--- 2343-query-kth-smallest-trimmed-number/2343-query-kth-smallest-trimmed-number.cpp
+++ 2343-query-kth-smallest-trimmed-number/2343-query-kth-smallest-trimmed-number.cpp
@@ -5,10 +5,10 @@ public:
         int n=nums.size();
         for(auto& q: queries)
         {
-            vector<pair<string,int>> pq;
+            vector<pair<string,int>> pq(n);
             for(int i=0;i<n;i++)
             {
-                pq.push_back({nums[i].substr(nums[i].size()-q[1]),i});
+                pq[i] = {nums[i].substr(nums[i].size()-q[1]),i};
             }
             sort(pq.begin(),pq.end());
             res.push_back(pq[q[0]-1].second);
