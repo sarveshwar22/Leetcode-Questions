0d0fe2ce4dd06acc0c7e65025eccb7923bc8462d
sarveshwar22 <56260368+sarveshwar22@users.noreply.github.com>
2022-09-07 09:47:07 +0530

    Time: 115 ms (79.55%), Space: 205.9 MB (27.03%) - LeetHub

---
+++ 2400-number-of-ways-to-reach-a-position-after-exactly-k-steps/2400-number-of-ways-to-reach-a-position-after-exactly-k-steps.cpp
@@ -0,0 +1,22 @@
+class Solution {
+    vector<vector<int>> dp;
+    int mod = 1e9+7;
+    int memo(int d, int k)
+    {
+        if(d>k)
+            return 0;
+        if(d==k)
+            return 1;
+        if(dp[d][k]!=0)
+            return dp[d][k]-1;
+        dp[d][k]=(1+memo(abs(d-1),k-1)+memo(d+1,k-1))%mod;
+        return dp[d][k]-1;
+        
+    }
+public:
+    int numberOfWays(int startPos, int endPos, int k) {
+        dp=vector<vector<int>>(1005,vector<int>(1001,0));
+        return memo(abs(endPos-startPos),k);
+        
+    }
+};
