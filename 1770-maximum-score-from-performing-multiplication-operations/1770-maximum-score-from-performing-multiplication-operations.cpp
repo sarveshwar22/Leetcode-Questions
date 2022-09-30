622367d2735fecea369da2706df18da8548772d0
sarveshwar22 <56260368+sarveshwar22@users.noreply.github.com>
2022-10-01 03:41:04 +0530

    Time: 163 ms (99.85%), Space: 77.2 MB (81.79%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
@@ -1,6 +1,6 @@
 class Solution {
     int dp[1001][1001];
-    int dfs(vector<int>& nums,vector<int>& mult, int i,int l,int r)
+    int dfs(vector<int>& nums,vector<int>& mult, int i=0,int l=0)
     {
         if(i==mult.size())
             return 0;
@@ -9,13 +9,15 @@ class Solution {
             return dp[l][i];
         }
         
-        int first = mult[i]*nums[l] +dfs(nums,mult,i+1,l+1,r);
-        int last = mult[i]*nums[r] + dfs(nums,mult,i+1,l,r-1);
+        int r = nums.size()-1 -(i-l);
+        
+        int first = mult[i]*nums[l] +dfs(nums,mult,i+1,l+1);
+        int last = mult[i]*nums[r] + dfs(nums,mult,i+1,l);
         return dp[l][i]=max(first,last);
     }
 public:
     int maximumScore(vector<int>& nums, vector<int>& multipliers) {
         memset(dp,-1,sizeof(dp));
-        return dfs(nums,multipliers,0,0,nums.size()-1);
+        return dfs(nums,multipliers,0,0);
     }
 };
