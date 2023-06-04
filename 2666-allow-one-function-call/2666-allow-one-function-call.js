dfc266730582981f8a21d76f3b916fdb8e2f4fc3
sarveshwar22 <56260368+sarveshwar22@users.noreply.github.com>
2023-06-04 22:49:16 +0530

    Time: 67 ms (11.25%), Space: 41.7 MB (81.28%) - LeetHub

---
+++ 2666-allow-one-function-call/2666-allow-one-function-call.js
@@ -0,0 +1,28 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+    let called = false;
+    let res;
+    return function(...args){
+        if(!called)
+            {
+                called = true;
+                res = fn(...args)
+                return res;
+            }
+        else
+            {
+                return undefined;
+            }
+    }
+};
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
