3c6cf734033d49690acc92b513e37221a07d03c6
sarveshwar22 <56260368+sarveshwar22@users.noreply.github.com>
2023-06-04 22:36:34 +0530

    Time: 66 ms (65.67%), Space: 45.2 MB (9.14%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,25 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let count = init;
+    return {
+        increment:function(){
+            return ++count;
+        },
+        decrement:function(){
+            return --count;
+        },
+        reset:function(){
+            return count=init;
+        }
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
