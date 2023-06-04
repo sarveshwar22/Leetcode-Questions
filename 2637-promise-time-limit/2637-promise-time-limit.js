6c938116db28e6919b071ece4213f600ae24da11
sarveshwar22 <56260368+sarveshwar22@users.noreply.github.com>
2023-06-04 20:47:20 +0530

    Time: 64 ms (40.73%), Space: 41.2 MB (97.76%) - LeetHub

---
+++ 2637-promise-time-limit/2637-promise-time-limit.js
@@ -0,0 +1,8 @@
+var timeLimit = function(fn, t) {
+  return async function(...args) {
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>reject("Time Limit Exceeded"),t);
+        fn(...args).then(resolve,reject);
+    })
+  };
+};
