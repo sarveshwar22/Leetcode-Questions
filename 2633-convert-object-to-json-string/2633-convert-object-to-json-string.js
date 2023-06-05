06aca1da4efc4be189669e68d1d402a7487b60a1
sarveshwar22 <56260368+sarveshwar22@users.noreply.github.com>
2023-06-05 09:31:43 +0530

    Time: 66 ms (85.88%), Space: 75 MB (46.85%) - LeetHub

--- 2633-convert-object-to-json-string/2633-convert-object-to-json-string.js
+++ 2633-convert-object-to-json-string/2633-convert-object-to-json-string.js
@@ -1,7 +1,26 @@
-/**
- * @param {any} object
- * @return {string}
- */
 var jsonStringify = function(object) {
-    return JSON.stringify(object)
+  // Handle null case
+  if (object === null) {
+    return 'null';
+  }
+
+  // Handle array case
+  if (Array.isArray(object)) {
+    const elements = object.map((elem) => jsonStringify(elem));
+    return `[${elements.join(',')}]`;
+  }
+
+  // Handle object case
+  if (typeof object === 'object') {
+    const keys = Object.keys(object);
+    const pairs = keys.map((key) => `"${key}":${jsonStringify(object[key])}`);
+    return `{${pairs.join(',')}}`;
+  }
+
+  // Handle primitive types
+  if (typeof object === 'string') {
+    return `"${object}"`;
+  }
+
+  return String(object); // Convert other types to string representation
 };
