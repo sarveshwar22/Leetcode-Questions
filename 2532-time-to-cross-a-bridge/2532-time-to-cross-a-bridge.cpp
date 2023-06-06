87476d4c957db5e5e8907037d7b4774f4002fe4d
sarveshwar22 <56260368+sarveshwar22@users.noreply.github.com>
2023-06-06 23:50:47 +0530

    Time: 150 ms (83.33%), Space: 22.2 MB (56.67%) - LeetHub

--- 2532-time-to-cross-a-bridge/2532-time-to-cross-a-bridge.cpp
+++ 2532-time-to-cross-a-bridge/2532-time-to-cross-a-bridge.cpp
@@ -1,45 +1,64 @@
 class Solution {
 public:
+    typedef pair<int, int> pii;
     int findCrossingTime(int n, int k, vector<vector<int>>& time) {
-        priority_queue<vector<int>> waiting;//time to ready,side,efficiency,index
-        priority_queue<vector<int>> ready;//side,efficiency,index
-        
-        for (int i = 0; i < k; i++)
-            waiting.push({0, 0, time[i][0] + time[i][2], i});
-        int timestamp = 0;
-        int remaining_boxes_on_right_side = n;
-        
-        while (n) {  
-            if (ready.empty() && timestamp < -waiting.top()[0])
-                timestamp = -waiting.top()[0];
-            
-            while (!waiting.empty() && timestamp >= -waiting.top()[0]) {
-                vector<int> curW = waiting.top();
-                waiting.pop();
-                ready.push({curW[1], curW[2],curW[3]});
+        // After sorting, the order of the indexes is the order of selection
+        stable_sort(time.begin(), time.end(), [](auto& x, auto& y) -> bool {
+            return x[0] + x[2] < y[0] + y[2];
+        });
+        priority_queue<int> lwait, rwait;
+        for (int i = 0; i < k; ++i) lwait.push(i);
+        // Maintain the finish time and index
+        priority_queue<pii, vector<pii>, greater<pii>> lwork, rwork;
+        int t = 0;
+        while (n || rwait.size() || rwork.size()) {
+            // Before t, these workers have finished their work
+            // Collect them and insert into waitlist
+            while (lwork.size() && lwork.top().first <= t) {
+                lwait.push(lwork.top().second);
+                lwork.pop();
             }
-            auto curR = ready.top();
-            ready.pop();
-            int side = curR[0];  
-            int i = curR[2];  
-            
-            if (side == 0) { 
-                if (remaining_boxes_on_right_side == 0)
-                    continue;
-                else
-                    remaining_boxes_on_right_side--;     
-                timestamp += time[i][0];                     
-            } 
+            while (rwork.size() && rwork.top().first <= t) {
+                rwait.push(rwork.top().second);
+                rwork.pop();
+            }
+
+            if (rwait.size() > 0) {
+                // Choose the rightside workers at first
+                int c = rwait.top();
+                rwait.pop();
+                // Restart to wait after cross and work
+                lwork.push(pii(t + time[c][2] + time[c][3], c));
+                // Bridge return to idle after worker crossed
+                t += time[c][2];
+            }
             else {
-            timestamp += time[i][2];       
-            n--;
+                // Nobody waiting in rightside, choose leftside workers
+                if (n == 0) {
+                    // No need to schedule leftside worker
+                    // Advance directly to the next worker to complete
+                    t = rwork.top().first; 
+                }
+                else {
+                    if (lwait.size() > 0) {
+                        // There are workers waiting in leftside 
+                        int c = lwait.top();
+                        lwait.pop();
+                        // Restart to wait after cross and work
+                        rwork.push(pii(t + time[c][0] + time[c][1], c));
+                        // Bridge return to idle after worker crossed
+                        t += time[c][0];
+                        n--;
+                    }
+                    else {
+                        // Choose the earlier complete time
+                        int u = lwork.size() ? lwork.top().first : INT_MAX;
+                        int v = rwork.size() ? rwork.top().first : INT_MAX;
+                        t = min(u, v);
+                    }
+                }
             }
-            
-            if (side == 1)
-                waiting.push({-(timestamp+time[i][3]),0,curR[1],i});
-            else
-                waiting.push({-(timestamp+time[i][1]),1,curR[1],i});
         }
-        return timestamp;
+        return t;
     }
 };
