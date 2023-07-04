73b3255859f3ed511ba2933ab5931656ae149cb6
sarveshwar22 <56260368+sarveshwar22@users.noreply.github.com>
2023-07-04 08:09:34 +0530

    Time: 166 ms (98.81%), Space: 90.6 MB (98.28%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
     void dfs(vector<vector<int>>& adj,int i, vector<int>& informTime, int &res,int curr)
     {
@@ -19,6 +20,27 @@ public:
         int res = 0;
         int curr = 0;
         dfs(adj,headID,informTime,res,curr);
+        return res;
+    }
+};
+*/
+class Solution {
+    int dfs(int i, vector<int>& manager, vector<int>& informTime)
+    {
+        if(manager[i]!=-1)
+        {
+            informTime[i]+=dfs(manager[i],manager,informTime);
+            manager[i]=-1;
+        }
+        return informTime[i];
+    }
+public:
+    int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
+        int res = 0;
+        for(int i=0;i<n;i++)
+        {
+            res = max(res,dfs(i,manager,informTime));
+        }
         return res;
     }
 };
