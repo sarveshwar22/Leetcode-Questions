6ba547bb0e281ff028e39cf99950a7f74fa58e47
sarveshwar22 <56260368+sarveshwar22@users.noreply.github.com>
2023-07-17 07:21:27 +0530

    Time: 51 ms (95.93%), Space: 41.9 MB (51.50%) - LeetHub

--- 2637-promise-time-limit/2637-promise-time-limit.js
+++ 2637-promise-time-limit/2637-promise-time-limit.js
@@ -2,13 +2,23 @@ var timeLimit = function(fn, t) {
   return async function(...args) {
       
       
-    return new Promise((resolve,reject)=>{
+    return new Promise(async(resolve,reject)=>{
         const id = setTimeout(()=>reject("Time Limit Exceeded"),t);
-        fn(...args)
-            .then((res)=>resolve(res))
-            .catch((err)=>reject(err))
-            .finally(() => clearTimeout(id))
-                  
+        // fn(...args)
+        //     .then((res)=>resolve(res))
+        //     .catch((err)=>reject(err))
+        //     .finally(() => clearTimeout(id))
+        try{
+            const res = await fn(...args)
+            resolve(res)
+        }
+        catch(err){
+            reject(err)
+        }
+        finally{
+            clearTimeout(id)
+        }
+        
     })
       
       
