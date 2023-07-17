ed7671042dc851bd6d09ea2e339c73715da32b7d
sarveshwar22 <56260368+sarveshwar22@users.noreply.github.com>
2023-07-17 06:46:34 +0530

    Time: 65 ms (27.61%), Space: 41.4 MB (80.61%) - LeetHub

--- 2621-sleep/2621-sleep.js
+++ 2621-sleep/2621-sleep.js
@@ -6,7 +6,13 @@ async function sleep(millis) {
     // function callback(resolve, reject){
     //     setTimeout(resolve,millis);
     // }
+    
+    //1
     return new Promise((resolve)=> setTimeout(resolve,millis))
+    
+    //2
+    await new Promise((resolve)=> setTimeout(resolve,millis))
+    
 }
 
 /** 
